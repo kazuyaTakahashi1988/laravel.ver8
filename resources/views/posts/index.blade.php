4b0e34a97e9fef0fe0e75d743b414c274f7b9594
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2022-06-19 21:44:06 +0900

    SET

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,7 @@
         <meta property="og:site_name" content="{{ config('app.name', 'welcome to Q & A site!') }}">
         <meta property="og:url" content="<?php echo $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>">
         <meta property="og:title" content="新着 Q & A">
-        <meta property="og:description" content='新着 Q & A'>
+        <meta property="og:description" content='「Q & A site - ナゼナゼの実 -」 は質問を投稿、及び回答することができるC to C サービスです。質問者の投稿に気軽にコメントとリプライを送ることができ、名前とメールアドレスの登録だけで簡単に始められます。'>
         <meta property="og:image" content="<?php echo $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']; ?>/ogp.jpg">
         <meta property="og:locale" content="ja_JP">
         <meta property="fb:admins" content="xxxxxxxxx">
