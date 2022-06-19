4162988083c237461dda9981fb95b5e15f2e9de5
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2022-06-19 20:46:18 +0900

    SET

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -9,20 +9,17 @@ class LikeController extends Controller
 {
     public function index()
     {
-        $likes = Like::latest()->where('user_id', \Auth::user()->id )->paginate(10);
+        $likes = Like::latest()->where('user_id', \Auth::user()->id)->paginate(10);
         $likes->load('post');
 
         return view('likes.index', [
-             'likes' => $likes,
+            'likes' => $likes,
         ]);
     }
     public function like($id)
     {
         $user_id = \Auth::user()->id;
-        $likeEven = Like::where('user_id', $user_id)->where('post_id', $id)->first();
-        if (empty($likeEven)) {
-            $like = Like::create(['post_id' => $id, 'user_id' => $user_id]);
-        }
+        Like::create(['post_id' => $id, 'user_id' => $user_id]);
         $likeCount = count(Like::where('post_id', $id)->get());
         return response()->json(['likeCount' => $likeCount]);
     }
