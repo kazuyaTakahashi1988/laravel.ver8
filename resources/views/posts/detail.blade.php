9965d8790f9dba533de4746a8599f6acd596b212
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2022-06-21 19:09:35 +0900

    SET

--- resources/views/posts/detail.blade.php
+++ resources/views/posts/detail.blade.php
@@ -10,7 +10,7 @@
         <meta property="og:url" content="<?php echo $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>">
         <meta property="og:title" content="{{ $post->title }} | {{ config('app.name', 'Q & A site - ナゼナゼの実 -') }}">
         <meta property="og:description" content='{{ mb_substr(str_replace(array("\r\n", "\r", "\n"), "", strip_tags($post->content)), 0, 130); }}'>
-        @if(asset('storage/image/'.$post->image))
+        @if($post->image)
         <meta property="og:image" content="{{ asset('storage/image/'.$post->image) }}">
         @else
         <meta property="og:image" content="<?php echo $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']; ?>/ogp.jpg">
